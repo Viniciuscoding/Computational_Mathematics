aa301a64b2aa62300add8d7bf602f2f1cf862a2b
Viniciuscoding <vin.idem@gmail.com>
2021-01-19 00:15:53 -0500

    Update LCM.py

--- LCM.py
+++ LCM.py
@@ -1,33 +1,17 @@
 """
 Least Common Multiplier: It is the smallest positive integer evenly divisible by all set numbers.
 """
-
-def lcm(n1, n2):
-    multiplier = 0
-    if n1 > n2:
-        multiplier = n1
-    else:
-        multiplier = n2
-        
-    while(True):
-        if multiplier % n1 == 0 and multiplier % n2 == 0:
-            break
+class lcm:
+    def lcm(n1, n2):
+        multiplier = 0
+        if n1 > n2:
+            multiplier = n1
         else:
-            multiplier += 1
-    return multiplie
-    
-    
-    
-def gcd(n1, n2):
-    # The Euclid's Algorithm.
-    while n2:      
-        n1, n2 = n2, n1 % n2
-    return n1
-    
-    
+            multiplier = n2
 
-    
-    
-    
-    
-   
+        while(True):
+            if multiplier % n1 == 0 and multiplier % n2 == 0:
+                break
+            else:
+                multiplier += 1
+        return multiplier
