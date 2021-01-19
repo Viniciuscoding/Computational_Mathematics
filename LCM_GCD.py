786b818898d981b5738ae171abfbca61162d1d76
Viniciuscoding <vin.idem@gmail.com>
2021-01-19 00:19:24 -0500

    Create LCM_GCD.py

---
+++ LCM_GCD.py
@@ -0,0 +1,18 @@
+"""
+Calculating the Least Common Multiplier with the Greatest Common Denominator
+"""
+import gcd
+
+def gcd(n1, n2):
+    # The Euclid's Algorithm.
+    while n2:      
+        n1, n2 = n2, n1 % n2
+    return n1
+
+def lcm_gcd(n1, n2):
+    return n1 * n2 // gcd(n1, n2)
+       
+# OR
+
+def lcm_gcd(n1, n2):
+    return n1 * n2 // gcd.gcd(n1, n2)
